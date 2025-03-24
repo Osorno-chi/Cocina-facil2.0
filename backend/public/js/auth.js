2b2a77aee4b2a8bc61a7a54223ed69cc21ccd248
Eduardo <eduardo.osocervantes@gmail.com>
2025-03-24 17:36:59 -0600

    FRONTENDURL

--- backend/public/js/auth.js
+++ backend/public/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = '/api';
 
 // ====================== FUNCIÓN DE LOGIN ======================
 const loginUser = async (email, password, captchaResponse) => {
